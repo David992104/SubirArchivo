b30c32637ca7d784e4260f026f58d2a1d59f01f7
David992104 <david.osornio99@gmail.com>
2018-02-03 18:49:36 -0600

    actualizacion de la clase subir

--- src/main/java/com/tesji/santander/subirarchivo/subir.java
+++ src/main/java/com/tesji/santander/subirarchivo/subir.java
@@ -4,4 +4,9 @@ package com.tesji.santander.subirarchivo;
 public class subir {
     private String nombre;
     private String direccion;
+    
+    
+    public String asignarNombre(String nombre){
+        return nombre;
+    }
 }
